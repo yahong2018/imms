056941e3c2db41893f77a657cde35955076c95b4
yahong2018 <SoftwareChina@qq.com>
2018-11-03 10:36:24 +0800

    支持jsp的原始框架

---
+++ src/main/java/com/zhxh/imps/ImmsApplication.java
@@ -0,0 +1,11 @@
+package com.zhxh.imps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImmsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/zhxh/imps/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.zhxh.imps.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+    @Value("${application.hello:Hello JSP}")
+    private String hello="Hello Jsp";
+
+    @RequestMapping(value={"/"})
+    public String index(Map<String,Object> model){
+        model.put("time",new Date());
+        model.put("message",this.hello);
+
+        return "index";
+    }
+}
