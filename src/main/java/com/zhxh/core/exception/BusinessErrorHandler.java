a76abb7fe3c76a00d601a8277da194178f7d364e
yahong2018 <SoftwareChina@qq.com>
2018-11-25 16:13:00 +0800

    代码优化

--- src/main/java/com/zhxh/core/exception/BusinessErrorHandler.java
+++ src/main/java/com/zhxh/core/exception/BusinessErrorHandler.java
@@ -1,15 +1,23 @@
 package com.zhxh.core.exception;
 
+import com.zhxh.core.utils.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class BusinessErrorHandler {
-
     @ExceptionHandler(BusinessException.class)
-    public String defaultExceptionHandler(HttpServletRequest request, Exception e) {
-        return "error/500-Business";
+    @ResponseStatus(HttpStatus.OK)
+    public ModelAndView defaultExceptionHandler(BusinessException e) {
+        Logger.error(e);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("message", e.getMessage());
+        modelAndView.setViewName("error/businessError");
+
+        return modelAndView;
     }
 }
--- src/main/java/com/zhxh/core/exception/BusinessException.java
+++ src/main/java/com/zhxh/core/exception/BusinessException.java
@@ -1,6 +1,6 @@
 package com.zhxh.core.exception;
 
-public class BusinessException extends Exception {
+public class BusinessException extends RuntimeException {
     public BusinessException(String message){
         super(message);
     }
